82bcc05884d10c2e377a7fc5795303570054d18f
tanish-aiplanet <tanish@aiplanet.com>
2024-04-23 19:31:10 +0530

    updated sonus-av-lib

--- code/sonus-av-lib/example/example_usage.py
+++ code/sonus-av-lib/example/example_usage.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 import sys
-sys.path.insert(0, 'E:/JUET/Projects/sonus/code/pylib')
+sys.path.insert(0, 'E:/JUET/Projects/sonus/code/sonus-av-lib')
 from sonus_av import AudioProcessor, ImageProcessor
 
 load_dotenv()
@@ -10,13 +10,13 @@ def main():
     # Example usage of AudioProcessor
     print("Demonstrating AudioProcessor:")
     audio_proc = AudioProcessor()
-    translated_text = audio_proc.capture_and_translate()
-    print(f"Translated Text: {translated_text}\n")
+    recognized_text = audio_proc.capture()
+    print(f"Recognized Text: {recognized_text}\n")
 
     # Example usage of ImageProcessor
     print("Demonstrating ImageProcessor:")
     image = os.environ.get("IMAGE_URL")
-    image_proc = ImageProcessor(openai_api_key=api_key)
+    image_proc = ImageProcessor(api_key=api_key)
     image_description = image_proc.describe_image(image)
     print(f"Image Description: {image_description}\n")
 
--- code/sonus-av-lib/sonus_av/audio.py
+++ code/sonus-av-lib/sonus_av/audio.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 from deep_translator import GoogleTranslator
+from google.transliteration import transliterate_text
 
 class AudioProcessor:
     def __init__(self, input_lang='auto', output_lang='en'):
@@ -7,7 +8,7 @@ class AudioProcessor:
         self.output_lang = output_lang
         self.recognizer = sr.Recognizer()
 
-    def capture_and_translate(self):
+    def capture(self):
         with sr.Microphone() as source:
             print("Listening...")
             audio = self.recognizer.listen(source)
@@ -19,7 +20,8 @@ class AudioProcessor:
 
                 # Translate to English
                 if self.output_lang != 'en':
-                    translated_text = GoogleTranslator(source=self.input_lang, target='en').translate(text=recognized_text)
+                    speech_text = transliterate_text(recognized_text, lang_code='en')
+                    translated_text = GoogleTranslator(source=self.input_lang, target=self.output_lang).translate(text=speech_text)
                     print(f"Translated to English: {translated_text}")
                     return translated_text
                 return recognized_text
